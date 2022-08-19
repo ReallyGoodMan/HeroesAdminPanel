6600cd0306a48ae05adb03f5dd87c57f091a80dd
ReallyGoodMan <104644020+ReallyGoodMan@users.noreply.github.com>
2022-08-19 10:22:26 +0300

    Delete src/actions directory

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-// import { filtersFetched, filtersFetching, filtersFetchingError } from "../components/heroesFilters/filtersSlice";
-
-// export const fetchFilter = (request) => (dispatch) => {
-//     dispatch(filtersFetching());
-//     request("http://localhost:3001/filters")
-//         .then(data => dispatch(filtersFetched(data)))
-//         .catch(() => dispatch(filtersFetchingError()))
-// }
-
